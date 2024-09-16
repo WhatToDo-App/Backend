ec19717ccdf06503c6be0112a6c4e653755ab0db
Abel Meseret <ameseret1990@gmail.com>
2024-09-16 21:22:06 +0300

    Add unit tests for Task API endpoints in tests.py

--- whattodo/tasks/tests.py
+++ whattodo/tasks/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Task
 
 # Create your tests here.
+class TaskAPITests(APITestCase):
+
+    def setUp(self):
+        self.task = Task.objects.create(title="Test Task", description="Test Description")
+
+    def test_get_tasks(self):
+        response = self.client.get('/api/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_task(self):
+        data = {'title': 'New Task', 'description': 'New Description'}
+        response = self.client.post('/api/tasks/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 2)
+
+    def test_update_task(self):
+        data = {'title': 'Updated Task'}
+        response = self.client.put(f'/api/tasks/{self.task.id}/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, 'Updated Task')
+
+    def test_delete_task(self):
+        response = self.client.delete(f'/api/tasks/{self.task.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.count(), 0)
