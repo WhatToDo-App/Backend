276294c89811c2aea001e24537b0f44fe216faf8
Abel Meseret <ameseret1990@gmail.com>
2024-09-21 21:30:30 +0300

    Edit CreateTask.js

--- whattodo/tasks/tests.py
+++ whattodo/tasks/tests.py
@@ -14,7 +14,18 @@ class TaskAPITests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-   
+    def test_create_task(self):
+        data = {'title': 'New Task', 'description': 'New Description'}
+        response = self.client.post('/api/tasks/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 2)
+
+    def test_update_task(self):
+        data = {'title': 'Updated Task'}
+        response = self.client.put(f'/api/tasks/{self.task.id}/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, 'Updated Task')
 
     def test_delete_task(self):
         response = self.client.delete(f'/api/tasks/{self.task.id}/')
