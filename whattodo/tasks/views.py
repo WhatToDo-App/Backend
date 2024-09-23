3a6d8e9d8808d7b66e831221cd263bd195fbb0dc
Abel Meseret <ameseret1990@gmail.com>
2024-09-23 19:20:01 +0300

    Add home view and update URL routing

--- whattodo/tasks/views.py
+++ whattodo/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -13,3 +14,6 @@ class TaskListCreateView(generics.ListCreateAPIView):
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+def home(request):
+    return HttpResponse("<h1>Welcome to the WhatToDo App!</h1><p><a href='/api/tasks/'>View Tasks</a></p>")
