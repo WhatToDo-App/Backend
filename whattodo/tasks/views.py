fe53a7f9c82a131d734c6fbc03b215cca62a7848
Abel Meseret <ameseret1990@gmail.com>
2024-10-05 16:05:53 +0300

    add authentication permissions to TaskViewSet and set user on task creation

--- whattodo/tasks/views.py
+++ whattodo/tasks/views.py
@@ -1,23 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import TaskSerializer
-
-# Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
+
+    def perform_create(self, serializer):
+        # Save the task with the currently authenticated user
+        serializer.save(user=self.request.user)
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]  # Optional if you want to enforce this here too
 
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]  # Optional
 
 def home(request):
     return HttpResponse("<h1>Welcome to the WhatToDo App!</h1><p><a href='/api/tasks/'>View Tasks</a></p>")
